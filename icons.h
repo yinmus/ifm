7de775b37b924fa1540f3ed941beb7e2544b5095
kliscle <lachin.akhmetov@gmail.com>
2025-03-17 09:19:20 +0500

    ICONS

--- icons.h
+++ icons.h
@@ -1,27 +1,4 @@
-// icons.h
-// by yinmus
-// MIT License
 
-// Copyright (c) 2025 IFM-YINMUS
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-// GITHUB: https://github.com/yinmus/ifm
 
 #ifndef ICONS_H
 #define ICONS_H
@@ -56,6 +33,7 @@ static const IconMapping icon_mappings[] = {
     {"cbz", ""},
     {"cc", ""},
     {"class", ""},
+    {"svg", ""},
     {"clj", ""},
     {"cljc", ""},
     {"cljs", ""},
@@ -131,8 +109,8 @@ static const IconMapping icon_mappings[] = {
     {"jar", ""},
     {"java", ""},
     {"jl", ""},
-    {"jpeg", ""},
-    {"jpg", ""},
+    {"jpeg", ""},
+    {"jpg", ""},
     {"js", ""},
     {"json", ""},
     {"jsonc", ""},
@@ -174,7 +152,7 @@ static const IconMapping icon_mappings[] = {
     {"php", ""},
     {"pl", ""},
     {"pm", ""},
-    {"png", ""},
+    {"png", ""},
     {"pp", ""},
     {"dps", ""},
     {"dpt", ""},
