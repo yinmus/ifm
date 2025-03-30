b140019e5ab526dc370a6acd390107a115b7ac05
kliscle <lachin.akhmetov@gmail.com>
2025-03-30 14:34:18 +0500

    src/ifm.h

--- src/ifm.h
+++ src/ifm.h
@@ -5,7 +5,7 @@
 
 Relative path : ifm/src/ifm.h
 Github url : https://github.com/yinmus/ifm.git
-License : GPLv3 
+License : GPLv3
 
 */
 
@@ -13,6 +13,7 @@ License : GPLv3
 #define IFM_H
 
 #include <time.h>
+
 #include "fmh.h"
 
 #define MAX_FILES 16384
@@ -28,50 +29,44 @@ extern char lpath[MAX_PATH];
 extern int s_hidden;
 
 typedef struct {
-    char path[MAX_PATH];
-    int marked;
+  char path[MAX_PATH];
+  int marked;
 } MarkedFile;
 
 extern MarkedFile marked_files[MAX_FILES];
 
 typedef struct {
-    char video_viewer[MAX_NAME];
-    char audio_viewer[MAX_NAME];
-    char image_viewer[MAX_NAME];
-    char default_viewer[MAX_NAME];
+  char video_viewer[MAX_NAME];
+  char audio_viewer[MAX_NAME];
+  char image_viewer[MAX_NAME];
+  char default_viewer[MAX_NAME];
 } Config;
 
 extern int last_clicked;
 extern time_t last_click_time;
 
-extern char **environ;
-
+extern char** environ;
 
 void create_default_config();
-void load_config(Config *config);
-void list(const char *path);    
-void rm(const char *path);
-void open_file(const char *filename);
+void load_config(Config* config);
+void list(const char* path);
+void rm(const char* path);
+void open_file(const char* filename);
 void cr_file();
 void cr_dir();
-void ren(const char *filename); 
-void open_with(const char *filename);
+void ren(const char* filename);
+void open_with(const char* filename);
 void to_back();
 void to_home();
 void UI();
 void show_marked_files();
 void mark_help();
-void fcontent(const char *filename);
+void fcontent(const char* filename);
 void doc_menu();
-int cpe(char *buffer, int max_len, const char *prompt);
-int confrim_delete(const char *filename);
-void get_info(const char *filename, char *info, size_t info_size);
+int cpe(char* buffer, int max_len, const char* prompt);
+int confrim_delete(const char* filename);
+void get_info(const char* filename, char* info, size_t info_size);
 void goto_cmd(int next_char);
-int dist_s(const char *str);
-int dirt(const char *path);
-void hs_path(const char *relative_path, char *absolute_path);
-
-
-
+void hs_path(const char* relative_path, char* absolute_path);
 
 #endif
