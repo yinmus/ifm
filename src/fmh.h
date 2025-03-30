fb555dc83de5b882bd20330c46792758abbf40c8
kliscle <lachin.akhmetov@gmail.com>
2025-03-30 14:31:36 +0500

    src/fmh.h | added function prototypes

--- src/fmh.h
+++ src/fmh.h
@@ -5,19 +5,19 @@
 
 Relative path : ifm/src/fmh.h
 Github url : https://github.com/yinmus/ifm.git
-License : GPLv3 
+License : GPLv3
 
 */
 
 #ifndef FMH_H
 #define FMH_H
 
-
 void reference();
 void Version();
 void goto_help();
 void cls();
-int compare(const void *a, const void *b);
-
+int compare(const void* a, const void* b);
+int dirt(const char* path);
+int dist_s(const char* str);
 
 #endif
