1fc462b503dec1716134c253c4ad1d0360e2edcf
kliscle <lachin.akhmetov@gmail.com>
2025-03-31 18:16:55 +0600

    Add files via upload

---
+++ src/ui.h
@@ -0,0 +1,18 @@
+/*
+======================================
+    IFM by yinmus (c) 2025-2025
+======================================
+
+Relative path : ifm/src/ui.h
+Github url : https://github.com/yinmus/ifm.git
+License : GPLv3
+
+*/
+
+#ifndef UI_H
+#define UI_H
+
+void UI();
+void show_marked_files();
+
+#endif
